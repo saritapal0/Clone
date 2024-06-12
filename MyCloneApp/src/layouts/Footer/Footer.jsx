6eca23785654ba5b498ac92f002ad41789ed5e7b
saritapal0 <159772730+saritapal0@users.noreply.github.com>
2024-06-12 18:41:51 +0530

    clone

--- MyCloneApp/src/layouts/Footer/Footer.jsx
+++ MyCloneApp/src/layouts/Footer/Footer.jsx
@@ -4,17 +4,22 @@ import {
   Typography,
   Grid,
   Card,
-  CardContent
+  CardContent,
+  Divider
 } from "@mui/material";
 import Googleplay from '../../assets/Images/Googleplay.png'
 import Appstore from '../../assets/Images/Appstore.png'
 import Logo from '../../../src/assets/Images/logo.svg';
-
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AbcIcon from '@mui/icons-material/Abc';
 
 
 const Footer = () => {
   return (
-    <Box sx={{ p: 6, bgcolor: '#fafafa', color: 'black', textAlign: 'left' }}>
+    <Box sx={{ p: 6, bgcolor: '#ede7f6', color: 'black', textAlign: 'left' }}>
       <Grid container spacing={0} sx={{ textAlign: 'left' }}> {/* Set textAlign to left */}
         <Grid item xs={12} md={6} lg={3}>
           <Typography variant="h6">Sobre nós</Typography>
@@ -50,22 +55,55 @@ const Footer = () => {
           </Typography>
         </Grid>
         <Grid item xs={12} md={6} lg={3}>
-  <Card sx={{ borderRadius: 8, bgcolor: '#283593', borderRadius: 0 }}>
-    <CardContent>
-      {/* Logo Image */}
-      <img src={Logo} alt="Logo" style={{ height: '25px', width: 'auto', filter: 'invert(100%)' }} />
-      {/* Card Content */}
-      <Box sx={{ mt: 2, color: "white" }}>
-        <Typography variant="body2">
-        Para proporcionar uma experiência inesquecível para quem precisa de um lar ou anuncia com a gente, o QuintoAndar aposta em design, segurança e tecnologia de ponta.
+          <Card sx={{ borderRadius: 8, bgcolor: '#283593', borderRadius: 0 }}>
+            <CardContent>
+              {/* Logo Image */}
+              <img src={Logo} alt="Logo" style={{ height: '25px', width: 'auto', filter: 'invert(100%)' }} />
+              {/* Card Content */}
+              <Box sx={{ mt: 2, color: "white" }}>
+                <Typography variant="body2">
+                  Para proporcionar uma experiência inesquecível para quem precisa de um lar ou anuncia com a gente, o QuintoAndar aposta em design, segurança e tecnologia de ponta.
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+     </Grid>
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' ,mt:8}}>
+        <Typography variant="body2" sx={{ flex: 1 }}>
+        Politica de privacidade 
         </Typography>
+        <Typography variant="body2" sx={{ flex: 1 }}>
+        Termos e condições de uso
+        </Typography>
+        <Typography variant="body2" sx={{ flex: 1 }}>
+        Política de Cookies
+        </Typography>
+        <Typography variant="body2" sx={{ flex: 1 }}>
+        Preferências de cookies
+        </Typography>
+        <Typography variant="body2" sx={{ flex: 1 }}>
+        Manual do usuário
+        </Typography>
+        <Box display="flex" justifyContent="flex-end">
+          <Box display="flex" alignItems="center" mr={1}>
+            < FacebookIcon />
+          </Box>
+          <Box display="flex" alignItems="center" mr={1}>
+            <InstagramIcon/>
+          </Box>
+          <Box display="flex" alignItems="center" mx={1}>
+            <TwitterIcon/>
+          </Box>
+          <Box display="flex" alignItems="center" mx={1}>
+            <LinkedInIcon/>
+          </Box>
+          <Box display="flex" alignItems="center" mx={1}>
+            <AbcIcon/>
+          </Box>
+        </Box>
       </Box>
-    </CardContent>
-  </Card>
-</Grid>
-
-      </Grid>
-      <Box sx={{ mt: 4 }}>
+      <Box sx={{ mt: 4 ,display: 'flex', flexDirection: 'row', alignItems: 'center',flex: 1}}>
         <Typography variant="body2">
           GRPQA Ltda. - CRECI-SP J24.344
         </Typography>
@@ -83,6 +121,7 @@ const Footer = () => {
           </Box>
         </Box>
       </Box>
+
     </Box>
   );
 }
